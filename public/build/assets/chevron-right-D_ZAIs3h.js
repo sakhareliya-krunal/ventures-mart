16bec51f3f9959a2b91f1b7ed82ff3623b146616
krunal-sakhareliya <sakhareliyakrunal03@gmail.com>
2026-08-28 21:40:57 +0530

    Build production assets

--- public/build/assets/chevron-right-D_ZAIs3h.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./app-Cnd-1wnf.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
---
+++ public/build/assets/chevron-right-j_B9VRaS.js
@@ -0,0 +1,1 @@
+import{M as e}from"./app-CZOKaNWj.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
